c637cde75693ebedebe9776febd683df4fea1a69
Sojibul Islam <programmersojib05@gmail.com>
2024-05-31 11:20:03 +0600

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,130 @@
+
+// 1. Create a variable called carName, assign the value Volvo to it.
+
+let habluName = "Volvo";
+console.log(habluName);
+
+
+// 2. On one single line, declare three variables with the following names and values:
+
+firstName = "John",
+lastName = "Doe",
+age = 35;
+
+console.log(firstName , lastName ,age)
+
+
+
+
+// 3. Use the correct assignment operator that will 
+
+
+x = 10;
+y = 5;
+x = x * y ;   // what will be the value of x ? 
+
+console.log(x)
+
+
+
+// 4.  Use comments to describe the correct data type of the following variables:
+
+let length = 16;   // number data type  
+let lastName = "Johnson"; // string data type 
+
+const x = {
+  firstName: "John",  
+  lastName: "Doe"
+};    // object data type
+
+
+
+
+// 5. Execute the function named myFunction.
+ 
+ function myFunction() {
+  alert("Hello World!");
+}
+
+myFunction();
+
+
+
+//  6. Create an object called person with name = John, age = 50, Then,
+//    access the object to alert("John is 50").
+
+
+const person = {
+    Name:"John",
+    age:50,
+}
+
+alert(person.Name + " is " + person.age)
+
+
+
+
+
+
+// 8. array question
+
+//  1. Alert the number of items in an array, using the correct Array property: 
+
+
+const sojib = ["Volvo", "Jeep", "Mercedes"];
+alert(sojib.length)
+
+
+
+// 2. Change the first item of Brand to "Ford".
+
+	const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+    Brand[0] = "arman";
+    console.log(Brand)
+
+
+
+
+
+// 9. Math Problem
+
+    //  1. Use the correct Math method to create a random number.
+
+    let a = Math.random();
+    console.log(a)
+
+
+
+    // 2. Use the correct Math method to return the largest number of 10 and 20.
+
+    let b = Math.max(10,20)
+    console.log(b)
+
+
+
+    //  3. Use the correct Math method to get the square root of 9.
+
+    let c = Math.sqrt(9);
+    console.log(c)
+
+
+
+
+
+    // 10. comparison operator problem
+
+    // 1. Choose the correct comparison operator to alert true, when x is greater than y. 
+    let d = 10;
+    let e = 5;
+    
+    alert( d > e) 
+    
+    
+    
+    // 2. Choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough".
+
+    var afe = n;
+    var alert = ( afe < 18 )? "Too young " : "old Enough"
+    alert(alert);
+    
+    
